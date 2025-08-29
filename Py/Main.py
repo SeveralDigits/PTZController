75c05873f2915b86e48ddae54893d8c0662acc51
SeveralDigits <xanderebs@gmail.com>
2025-08-29 18:55:49 +0200

    Released main code. Including Prebuilt libraries

---
+++ Py/Main.py
@@ -0,0 +1,60 @@
+import clr
+clr.AddReference(r"C:\Users\Alexander\Desktop\LOGITECH PTZ\Lib\PTZ2.dll")
+from PTZ import PTZDevice, PTZType
+from tkinter import *
+
+camera_name = "Logi Group Camera"  # adjust to your camera name
+
+# Create device
+device = PTZDevice.GetDevice(camera_name, PTZType.Relative)
+
+# Print zoom info
+print("Zoom range:", device.ZoomMin, "to", device.ZoomMax, "step:", device.ZoomStep)
+
+# Move camera
+#device.Move(50, 0)   # pan right
+#device.Move(0, 50)   # tilt down
+
+#print("New zoom:", device.Zoom(1))
+#print("New zoom:", device.Zoom(-1))
+
+root = Tk()
+root.title("PTZ Controller")
+root.geometry('500x200')
+
+labelStep = Label(root, text="Step amount")
+entryStep = Entry(root)
+
+def ZoomIn():
+    Amnt = int(entryStep.get())
+    device.Zoom(Amnt)
+
+def ZoomOut():
+    Amnt = int(entryStep.get())
+    device.Zoom(-Amnt)
+
+def PanLeft():
+    Amnt = int(entryStep.get())
+    device.Move(-Amnt, 0)
+
+def PanRight():
+    Amnt = int(entryStep.get())
+    device.Move(Amnt, 0)
+
+def TiltUp():
+    Amnt = int(entryStep.get())
+    device.Move(0, -Amnt)
+
+def TiltDown():
+    Amnt = int(entryStep.get())
+    device.Move(0, Amnt)
+
+buttonUp = Button(root, text="Up", command=TiltUp)
+buttonDown = Button(root, text="Down", command=TiltDown)
+buttonLeft = Button(root, text="Left", command=PanLeft)
+buttonRight = Button(root, text="Right", command=PanRight)
+
+buttonZoomIn = Button(root, text="Zoom in", command=ZoomIn)
+buttonZoomOut = Button(root, text="Zoom out", command=ZoomOut)
+
+root.mainloop()
